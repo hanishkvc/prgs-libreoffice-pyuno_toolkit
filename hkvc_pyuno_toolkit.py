857a7332ff5233ccc82a57f81ced612d244845e6
hanishkvc <hanishkvc@gmail.com>
2020-04-18 01:59:00 +0530

    Got details of the document and the sheets in a spreadsheet
    However it appears like I cant get the actual number of active
    rows and columns in a spreadsheet
    
    NOTE: Post facto commit

--- hkvc_pyuno_toolkit.py
+++ hkvc_pyuno_toolkit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # A simple pyuno helper toolkit/library
-# v20200418IST0100, HanishKVC
+# v20200418IST0157, HanishKVC
 #
 
 import os
 import uno
+import time
 
 
 def oo_run():
@@ -25,7 +26,30 @@ def oo_connect():
     return desktop
 
 
-oo_run()
-oo = oo_connect()
-print(dir(oo))
+def oo_opendoc(desktop, filePath):
+    document = desktop.loadComponentFromURL(uno.systemPathToFileUrl(os.path.abspath(filePath)), "_blank", 0, ())
+    return document
+
+
+def oo_getsheets(document):
+    controller = document.getCurrentController()
+    sheets = document.getSheets()
+    return sheets, controller
+
+
+if __name__ == "__main__":
+    oo_run()
+    time.sleep(2)
+    oo = oo_connect()
+    doc = oo_opendoc(oo, "/tmp/t.xlsx")
+    sheets, ctlr = oo_getsheets(doc)
+    print("NumRows", len(sheets[0].getRows()), "\n Rows", sheets[0].getRows())
+    print("NumCols", len(sheets[0].getColumns()), "\n Cols", sheets[0].getColumns())
+    for sheet in sheets:
+        print("Sheet", dir(sheet))
+        # sheet.NamedRanges, sheet.getRows(), sheet.Rows, sheet.getColumns
+        # sheet.NamedRanges['MyRange'], sheet.NamedRanges.getByName('MyRange')
+        # sheet.getPrintAreas(), sheet.showDetail(?)
+        print("Cell", dir(sheet.getCellByPosition(5,5)))
+        print(sheet.getCellByPosition(5,5).getValue())
 
